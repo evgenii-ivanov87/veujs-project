d043c17d5f4b86cc1a114c1ef3026e79235bf38b
evgenii-ivanov87 <66361345+evgenii-ivanov87@users.noreply.github.com>
2022-04-06 21:56:48 +0300

    Done

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <Notifications />
+    <div class="content">
+      <AppHeader />
+      <router-view></router-view>
+    </div>
+    <AppFooter />
+  </div>
+</template>
+
+<script>
+import AppFooter from './components/Footer';
+import AppHeader from './components/header';
+import Notifications from './components/notifications';
+
+export default {
+  name: 'App',
+  components: {
+    AppFooter,
+    AppHeader,
+    Notifications,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  font-family: Montserrat, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.content {
+  flex-grow: 1;
+}
+</style>
